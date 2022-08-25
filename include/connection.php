bac082c75256339ee1fb80239ef01dca499c8e8e
balayokesh <balayokeshmani@gmail.com>
2022-08-25 17:57:35 +0530

    updated DB connection details

--- include/connection.php
+++ include/connection.php
@@ -3,6 +3,6 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'massistance');
+define('DB_DATABASE', 'yourfriends');
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 
